bb375f922e2220fad701245056918c3c8a0e15be
pantelis <pantelis.tsagkas@jet2.com>
2025-05-02 12:46:12 +0100

    Changed the color to blue(h1)

--- simple-web-server/server.js
+++ simple-web-server/server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
                 box-shadow: 0 2px 4px rgba(0,0,0,0.1);
             }
             h1 {
-                color:rgb(206, 118, 18);
+                color:rgb(99, 18, 206);
                 border-bottom: 2px solid #3498db;
                 padding-bottom: 10px;
             }
